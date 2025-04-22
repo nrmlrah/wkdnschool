c253d80b9500b9467f36acd56be615cadc108f7e
nrmlrah <nirmal@crackedhq.com>
2025-04-22 15:39:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 function App() {
-
   return (
     <>
       <style>
@@ -12,38 +11,36 @@ function App() {
           }
         `}
       </style>
-      <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
+      <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', position: 'relative' }}>
         <iframe
-          src="https://wall.page/9qa7ej" // Replace with your own URL
+          src="https://wall.page/9qa7ej"
           title="Embedded Webpage"
           style={{ width: '100%', height: '100%', border: 'none' }}
         />
-
-{/*         <button style={{ // Button to hide the framer badge, replace with your own CTA button
-          position: 'absolute',
-          bottom: '17px',
-          right: '33px',
-          width: '145px',
-          height: '40px',
-          backgroundColor: 'black',
-          color: "white",
-          borderRadius: '9px',
-          border: 'none',
-          cursor: 'pointer',
-          fontSize: '14px',
-          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-        }}
+        <button
+          style={{
+            position: 'absolute',
+            bottom: '17px',
+            right: '33px',
+            width: '145px',
+            height: '40px',
+            backgroundColor: 'black',
+            color: 'white',
+            borderRadius: '9px',
+            border: 'none',
+            cursor: 'pointer',
+            fontSize: '14px',
+            boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+          }}
           onClick={() => {
-            window.open('https://calendly.com/nirmal-pebbl/glitch-reel-demo', '_blank')
-          }} */}
+            window.open('https://calendly.com/nirmal-pebbl/glitch-reel-demo', '_blank');
+          }}
         >
           Book Demo
         </button>
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
-// __framer-badge-container
+export default App;
